67ae2324aa8b4b5251964a892adf9b9877645a76
royhan31 <izzapark34@gmail.com>
2019-12-16 12:41:11 +0700

    lamp

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var five = require("johnny-five");
+var io = require('socket.io-client');
+var socket = io.connect('http://plug-plant.herokuapp.com/');
+var board = new five.Board();
+
+board.on("ready", function(){
+  const led = new five.Led(12);
+  socket.on('all tools', function(data){
+    for (var i = 0; i < data.length; i++) {
+      if(data[i].lamp){
+        led.on()
+      }else {
+        led.off()
+      }
+    }
+  });
+});
